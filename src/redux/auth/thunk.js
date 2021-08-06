fd06baf6ef6f10e30c12f4269579c8b6dafbc677
Gohar Attiq <gohar.attiq@arbisoft.com>
2021-08-06 22:12:08 +0500

    proxy bug fixed

--- src/redux/auth/thunk.js
+++ src/redux/auth/thunk.js
@@ -1,12 +1,13 @@
 /* eslint-disable import/prefer-default-export */
 import axios from 'axios';
-import { BASE_URL } from '../constants';
-import { signInSueccess } from './actions';
+import { signInSueccess, signUpSueccess } from './actions';
 
 export const signinUser = ({ email, password }) => (dispatch) => {
   const locale = navigator.language.slice(0, 2);
   axios
-    .post(`${BASE_URL}/api/users/signin`, { email, password, locale })
+    .post('/api/users/signin', {
+      email, password, locale,
+    })
     .then((res) => {
       // console.log(res.data);
       dispatch(signInSueccess(res.data));
@@ -20,11 +21,11 @@ export const signinUser = ({ email, password }) => (dispatch) => {
 export const signupUser = ({ username, email, password }) => (dispatch) => {
   const locale = navigator.language.slice(0, 2);
   axios
-    .post(`${BASE_URL}/api/users/signup`, {
+    .post('/api/users/signup', {
       username, email, password, locale,
     })
-    .then(async (res) => {
-      dispatch(signInSueccess(res.data));
+    .then((res) => {
+      dispatch(signUpSueccess(res.data));
     })
     .catch((err) => {
       // eslint-disable-next-line no-console
