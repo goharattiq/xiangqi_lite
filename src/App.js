eb88136199aa1e020025bf4287c2dfae17a13894
Gohar Attiq <gohar.attiq@arbisoft.com>
2021-08-06 14:30:58 +0500

    redux store created and connected

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 /* eslint-disable react/jsx-filename-extension */
 import React from 'react';
+import { Provider } from 'react-redux';
+import store from './redux/store';
 import Authentication from './Authentication/Authentication';
 
 const App = () => (
-  <Authentication />
+  <Provider store={store}>
+    <Authentication />
+  </Provider>
+
 );
 
 export default App;
---
+++ src/redux/store.js
@@ -0,0 +1,19 @@
+/* eslint-disable no-underscore-dangle */
+import { createStore, applyMiddleware, compose } from 'redux';
+import thunk from 'redux-thunk';
+import rootReducer from './rootReducer';
+
+const initialState = {};
+const middleware = [thunk];
+
+const reduxCompose = window.__REDUX_DEVTOOLS_EXTENSION__
+  ? compose(applyMiddleware(...middleware), window.__REDUX_DEVTOOLS_EXTENSION__())
+  : applyMiddleware(...middleware);
+
+const store = createStore(
+  rootReducer,
+  initialState,
+  reduxCompose,
+);
+
+export default store;
