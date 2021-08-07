b025a9ef02502bdf03e562d18f9e7d750ffa172c
Gohar Attiq <gohar.attiq@arbisoft.com>
2021-08-07 15:49:25 +0500

    profile reducer added

--- src/redux/rootReducer.js
+++ src/redux/rootReducer.js
@@ -1,6 +1,8 @@
 import { combineReducers } from 'redux';
 import authReducer from './auth/reducer';
+import profileReducer from './profile/redux';
 
 export default combineReducers({
   auth: authReducer,
+  profile: profileReducer,
 });
