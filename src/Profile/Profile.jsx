e551667ef2c571b20df9eda64cd9eb5c5628bfe4
Gohar Attiq <gohar.attiq@arbisoft.com>
2021-08-07 15:28:12 +0500

    profile page ui added

--- src/Profile/Profile.jsx
+++ src/Profile/Profile.jsx
@@ -1,16 +1,37 @@
 import React from 'react';
 import { Container } from 'react-bootstrap';
+import './Profile.scss';
 
-const Profile = () => {
-  const activeTab = 0;
-  return (
-    <Container>
-      <div>
-        <p>{activeTab}</p>
-        <p>goahrattiq</p>
-      </div>
-    </Container>
-  );
-};
+const Profile = () => (
+  <Container className="bg-white w-75 mt-5">
+    <div className="d-inline-flex user-profile mt-5 ms-5">
+      <div className="avatar" />
+      <p className="user-fullname">Gohar Attiq</p>
+      <p className="user-username">goharattiq</p>
+    </div>
+    <ul className="m-5 list-group list-group-horizontal user-stats">
+      <li className="score list-group-item m-2">
+        <h6 className="ps-2">23</h6>
+        <p>Games</p>
+      </li>
+      <li className="score list-group-item m-2">
+        <h6 className="ps-2">23</h6>
+        <p>Wins</p>
+      </li>
+      <li className="score list-group-item m-2">
+        <h6 className="ps-2">23</h6>
+        <p>Losses</p>
+      </li>
+      <li className="score list-group-item m-2">
+        <h6 className="ps-2">23</h6>
+        <p>Draws</p>
+      </li>
+      <li className="score list-group-item m-2">
+        <h6 className="ps-2">23%</h6>
+        <p>Winning</p>
+      </li>
+    </ul>
+  </Container>
+);
 
 export default Profile;
