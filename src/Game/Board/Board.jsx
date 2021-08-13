3d33dc9b9a2f5bebe59a7247f73988f1a19e908b
Gohar Attiq <gohar.attiq@arbisoft.com>
2021-08-13 15:53:34 +0500

    game board row componentization

--- src/Game/Board/Board.jsx
+++ src/Game/Board/Board.jsx
@@ -4,9 +4,10 @@
 /* eslint-disable react/jsx-props-no-spreading */
 import React from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { DragDropContext, Draggable, Droppable } from 'react-beautiful-dnd';
-import './Board.scss';
+import { DragDropContext } from 'react-beautiful-dnd';
 import { pieceMove } from '../../redux/game/actions';
+import Row from './Row';
+import './Board.scss';
 
 const Board = () => {
   const { board } = useSelector(({ game }) => ({ board: game.board }));
@@ -21,80 +22,15 @@ const Board = () => {
         <DragDropContext
           onDragEnd={(move) => onDragEnd(move)}
         >
-          {board.map((row, rowIndex) => (
-            <tr
-              key={rowIndex}
-            >
-              {
-                row.map((cell) => (
-                  <td>
-                    <Droppable droppableId={`droppable-${cell.id}`} key={cell.id}>
-                      {(provided) => (
-                        <div
-                          {...provided.droppableProps}
-                          ref={provided.innerRef}
-                          className="droppable"
-                        >
-                          {
-                            cell.item
-                              ? (
-                                <Draggable
-                                  draggableId={`dragable-${cell.id}`}
-                                  index={cell.id}
-                                  key={cell.id}
-                                >
-                                  {(provid) => (
-                                    <div
-                                      ref={provid.innerRef}
-                                      {...provid.draggableProps}
-                                      {...provid.dragHandleProps}
-                                      className="drag-div"
-                                    />
-                                  )}
-                                </Draggable>
-                              ) : ''
-                          }
-                          {provided.placeholder}
-                        </div>
-                      )}
-                    </Droppable>
-                  </td>
-                ))
-              }
-              {/* <div>
-                <Droppable droppableId={`droppable-${rowIndex}`} key={rowIndex}>
-                  {(provided) => (
-                    <div
-                      {...provided.droppableProps}
-                      ref={provided.innerRef}
-                      className="droppable"
-                    >
-                      {row.map((cell, index) => (
-                        cell !== '-'
-                          ? (
-                            <Draggable
-                              key={index}
-                              draggableId={`drag-${index}`}
-                              index={index}
-                            >
-                              {(provid) => (
-                                <div
-                                  ref={provid.innerRef}
-                                  {...provid.draggableProps}
-                                  {...provid.dragHandleProps}
-                                  className="position-absolute block drag-div"
-                                />
-                              )}
-                            </Draggable>
-                          ) : ''
-                      ))}
-                      {provided.placeholder}
-                    </div>
-                  )}
-                </Droppable>
-              </div> */}
-            </tr>
-          ))}
+          {
+            board.map((row, rowIndex) => (
+              <tr
+                key={rowIndex}
+              >
+                <Row row={row} key={rowIndex} />
+              </tr>
+            ))
+          }
         </DragDropContext>
       </tbody>
     </table>
---
+++ src/Game/Board/Row.jsx
@@ -0,0 +1,44 @@
+/* eslint-disable react/no-array-index-key */
+/* eslint-disable react/jsx-props-no-spreading */
+import React from 'react';
+import { Draggable, Droppable } from 'react-beautiful-dnd';
+
+const Row = ({ row }) => (
+
+  row.map((cell, cellIndex) => (
+    <td key={`tr-${cellIndex}`}>
+      <Droppable droppableId={`droppable-${cell.id}`} key={cell.id}>
+        {(provided) => (
+          <div
+            {...provided.droppableProps}
+            ref={provided.innerRef}
+            className="droppable"
+          >
+            {
+                cell.item
+                  ? (
+                    <Draggable
+                      draggableId={`dragable-${cell.id}`}
+                      index={cell.id}
+                      key={cell.id}
+                    >
+                      {(provid) => (
+                        <div
+                          ref={provid.innerRef}
+                          {...provid.draggableProps}
+                          {...provid.dragHandleProps}
+                          className="drag-div"
+                        />
+                      )}
+                    </Draggable>
+                  ) : ''
+              }
+            {provided.placeholder}
+          </div>
+        )}
+      </Droppable>
+    </td>
+  ))
+);
+
+export default Row;
