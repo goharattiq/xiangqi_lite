be37e164c92a17ae1c65f11855ca6b0f41e1ccda
Gohar Attiq <gohar.attiq@arbisoft.com>
2021-08-19 16:54:21 +0500

    chat reducer connected

--- src/Game/Chat.jsx
+++ src/Game/Chat.jsx
@@ -1,20 +1,68 @@
-import React from 'react';
-import { FormControl, InputGroup } from 'react-bootstrap';
+/* eslint-disable react/no-array-index-key */
+import React, { useState } from 'react';
+
+import {
+  Button, Form, FormControl, InputGroup,
+} from 'react-bootstrap';
+import { useDispatch, useSelector } from 'react-redux';
+import { messageSend } from '../redux/chat/actions';
 import './Chat.scss';
 
-const Chat = () => (
-  <div className="chat position-relative">
-    <div className="message-history" />
-    <InputGroup className="type_msg mb-3">
-      <FormControl
-        placeholder="Type a message"
-        aria-label="Username"
-        aria-describedby="basic-addon1"
-      />
-      <InputGroup.Text className="btn btn-primary" id="basic-addon1"><i className="fas fa-paper-plane" /></InputGroup.Text>
-    </InputGroup>
-  </div>
+const Chat = () => {
+  const dispatch = useDispatch();
+  const chat = useSelector((state) => state.chat);
+  const [message, setMessage] = useState({
+    content: '',
+    author: '',
+  });
+  const { content } = message;
+  const handleChange = ({ target: { name, value } }) => {
+    setMessage({
+      ...message,
+      [name]: value,
+      author: 'goharattiq',
+    });
+  };
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    dispatch(messageSend(message));
+    setMessage({
+      content: '',
+      author: '',
+    });
+  };
+  return (
+    <div className="chat position-relative">
+      <div className="message-history">
+        {
+          // eslint-disable-next-line no-shadow
+          chat.length !== 0 ? chat.map(({ content, author }, index) => (
+            <div className="message" key={index}>
+              <p>
+                {content}
+              </p>
+              <span className="author">
+                {author}
+              </span>
+            </div>
+          )) : ''
+        }
+      </div>
+      <Form onSubmit={handleSubmit}>
+        <InputGroup className="type_msg mb-3">
+          <FormControl
+            type="text"
+            name="content"
+            value={content}
+            onChange={handleChange}
+            placeholder="Type a message"
+          />
+          <Button className="btn btn-primary" id="basic-addon1" type="submit"><i className="fas fa-paper-plane" /></Button>
+        </InputGroup>
+      </Form>
+    </div>
 
-);
+  );
+};
 
 export default Chat;
--- src/redux/rootReducer.js
+++ src/redux/rootReducer.js
@@ -1,5 +1,6 @@
 import { combineReducers } from 'redux';
 import authReducer from './auth/reducer';
+import chatReducer from './chat/reducer';
 import gameReducer from './game/reducer';
 import leaderReducer from './leaderboard/reducer';
 import profileReducer from './profile/redux';
@@ -9,4 +10,5 @@ export default combineReducers({
   profile: profileReducer,
   game: gameReducer,
   leaderBoard: leaderReducer,
+  chat: chatReducer,
 });
