863fbe07d51a89c28fbc8f32f60b5f65b7a173b2
Gohar Attiq <gohar.attiq@arbisoft.com>
2021-08-19 15:10:32 +0500

    chat box collapse added

--- src/Game/Game.jsx
+++ src/Game/Game.jsx
@@ -1,19 +1,38 @@
 import React from 'react';
+import { useMediaQuery } from 'react-responsive';
+import { Button } from 'react-bootstrap';
 import Chat from './Chat';
 import PlayArea from './PlayArea';
 import './Game.scss';
 
 const Game = () => {
+  // eslint-disable-next-line no-unused-vars
+  const isTablet = useMediaQuery({ query: '(max-width: 880px)' });
   document.body.style.backgroundColor = '#be342d';
   return (
-    <div className="row">
-      <div className="col-md-8 col-sm-12 col-xs-12">
-        <PlayArea />
+    <>
+      <div className="row">
+        <div className="col-md-8 col-sm-12 col-xs-12">
+          <PlayArea />
+        </div>
+        <div className={`${!isTablet ? 'collapse show col-md-4' : 'collapse overlay-chatbox'}`} id="collapse-coloumn">
+          <Chat />
+        </div>
       </div>
-      <div className="col-md-4 chat-area">
-        <Chat />
-      </div>
-    </div>
+      {
+        isTablet ? (
+          <Button
+            className="custom-btn"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#collapse-coloumn"
+          >
+            <i className="fas fa-comment" />
+          </Button>
+        ) : ''
+      }
+
+    </>
   );
 };
 
