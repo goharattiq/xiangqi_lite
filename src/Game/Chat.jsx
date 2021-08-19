db9528937d40da7bb26ab69f4c0a2e5fdcc90900
Gohar Attiq <gohar.attiq@arbisoft.com>
2021-08-19 16:28:55 +0500

    chat box ui updated

--- src/Game/Chat.jsx
+++ src/Game/Chat.jsx
@@ -1,8 +1,20 @@
 import React from 'react';
+import { FormControl, InputGroup } from 'react-bootstrap';
 import './Chat.scss';
 
 const Chat = () => (
-  <div className="chat" />
+  <div className="chat position-relative">
+    <div className="message-history" />
+    <InputGroup className="type_msg mb-3">
+      <FormControl
+        placeholder="Type a message"
+        aria-label="Username"
+        aria-describedby="basic-addon1"
+      />
+      <InputGroup.Text className="btn btn-primary" id="basic-addon1"><i className="fas fa-paper-plane" /></InputGroup.Text>
+    </InputGroup>
+  </div>
+
 );
 
 export default Chat;
