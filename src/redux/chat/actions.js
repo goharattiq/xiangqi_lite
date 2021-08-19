fa73f66cd3fac6cdbe7ebdc21bbbed04a5ef3413
Gohar Attiq <gohar.attiq@arbisoft.com>
2021-08-19 16:29:12 +0500

    chat reducer added

---
+++ src/redux/chat/actions.js
@@ -0,0 +1,9 @@
+/* eslint-disable import/prefer-default-export */
+import {
+  MESSAGE_SEND,
+} from './type';
+
+export const messageSend = (message) => ({
+  type: MESSAGE_SEND,
+  payload: message,
+});
