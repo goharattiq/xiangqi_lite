58d0e8d0ab90e4361275e879a67b7e6b425009b3
Gohar Attiq <gohar.attiq@arbisoft.com>
2021-08-29 12:53:56 +0500

    lobby socket connection added

--- src/Lobby/Lobby.jsx
+++ src/Lobby/Lobby.jsx
@@ -1,11 +1,42 @@
-import React, { useState } from 'react';
-import { Button } from 'react-bootstrap';
+import React, { useState, useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { useHistory } from 'react-router-dom';
+import { Button, FloatingLabel, Form } from 'react-bootstrap';
 import GameParams from './GameParams';
+import { gameParamsAct } from '../redux/game/actions';
+import { socketEnterGame, useSockets } from '../scoketio/socketio';
 import './Lobby.scss';
 
 const Lobby = () => {
   document.body.style.backgroundColor = '#ede8e0';
   const [overlayDiv, setOverlayDiv] = useState(false);
+  const { user } = useSelector(({ auth }) => (
+    { user: auth.user }));
+  const dispatch = useDispatch();
+
+  const setGameParams = (params) => {
+    dispatch(gameParamsAct(params));
+  };
+  const history = useHistory();
+  useEffect(() => {
+    // const disconnectSocket = useSockets(SOCKET_URL, 'opop', setGameParams);
+    useSockets(
+      'opop', setGameParams, history, user.username, dispatch,
+    );
+    // return () => {
+    //   disconnectSocket();
+    // };
+  }, [useSockets]);
+
+  const [gameId, setGameID] = useState('');
+  const handleChange = ({ target: { value } }) => {
+    setGameID(value);
+  };
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    socketEnterGame(gameId);
+    setGameID('');
+  };
   return (
     <>
       <Button className="position-absolute m-2 new-game" onClick={() => { setOverlayDiv(!overlayDiv); }}>
@@ -13,6 +44,26 @@ const Lobby = () => {
         New Game
       </Button>
       {overlayDiv ? <GameParams setOverlayDiv={setOverlayDiv} /> : ''}
+
+      <Form className="sign-form" onSubmit={handleSubmit}>
+        <FloatingLabel
+          label="game ID"
+        >
+          <Form.Control
+            type="text"
+            name="gameId"
+            value={gameId}
+            placeholder="Game ID"
+            onChange={handleChange}
+          />
+        </FloatingLabel>
+        <Button
+          className="form-button"
+          type="submit"
+        >
+          Enter Game
+        </Button>
+      </Form>
     </>
   );
 };
