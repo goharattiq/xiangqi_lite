f50d311b3261dd74588a5895d174dbc090120c59
Gohar Attiq <gohar.attiq@arbisoft.com>
2021-08-30 22:45:56 +0500

    active games component added

---
+++ src/Lobby/ActiveGame.jsx
@@ -0,0 +1,25 @@
+/* eslint-disable jsx-a11y/no-static-element-interactions */
+/* eslint-disable jsx-a11y/click-events-have-key-events */
+import React from 'react';
+import { useSelector } from 'react-redux';
+import { socketEnterGame } from '../scoketio/socketio';
+
+const ActiveGame = () => {
+  const activeGames = useSelector(({ game }) => (game.activeGames));
+  return (
+    <div>
+      <h2>My Active Games</h2>
+      {
+        activeGames.length !== 0
+          ? activeGames.map((game) => (
+            <div key={game.id} onClick={() => { socketEnterGame(game.id); }}>
+              <p>{game.side}</p>
+            </div>
+          ))
+          : <p>No Active Games</p>
+      }
+    </div>
+  );
+};
+
+export default ActiveGame;
