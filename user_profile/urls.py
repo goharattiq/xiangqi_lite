09788e3f16fc0326c75ad7c033c6c0593e85061e
Gohar Attiq <gohar.attiq@arbisoft.com>
2021-08-30 16:32:15 +0500

    search user view added and url register

--- user_profile/urls.py
+++ user_profile/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import RetrieveProfile, CreateProfile
+from .views import RetrieveProfile, CreateProfile, SearchUser
 
 urlpatterns = [
     path('', CreateProfile.as_view()),
     path('<pk>/', RetrieveProfile.as_view()),
+    path('search/<username>/', SearchUser.as_view()),
 ]
--- user_profile/views.py
+++ user_profile/views.py
@@ -1,8 +1,10 @@
-from rest_framework.generics import RetrieveUpdateAPIView, CreateAPIView
+from django.contrib.auth.models import User
+from rest_framework.generics import RetrieveUpdateAPIView, CreateAPIView, ListAPIView
 from rest_framework.generics import get_object_or_404
 from rest_framework.permissions import IsAuthenticated
+from django.db.models import Q
 from .models import Profile
-from .serializers import ProfileSerializer
+from .serializers import ProfileSerializer, UserSearchSerializer
 
 
 class RetrieveProfile(RetrieveUpdateAPIView):
@@ -18,3 +20,11 @@ class CreateProfile(CreateAPIView):
     permission_classes = [IsAuthenticated]
     queryset = Profile.objects.all()
     serializer_class = ProfileSerializer
+
+
+class SearchUser(ListAPIView):
+    permission_classes = [IsAuthenticated]
+    serializer_class = UserSearchSerializer
+
+    def get_queryset(self):
+        return User.objects.filter(Q(username__startswith=self.kwargs.get('username')))
