eae1bdd404aaaf6e33db6f4d2560fd16e5d416b0
Gohar Attiq <gohar.attiq@arbisoft.com>
2021-09-13 11:34:21 +0500

    direct url issue fixed

--- xiangqi_django/urls.py
+++ xiangqi_django/urls.py
@@ -1,16 +1,17 @@
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include,re_path
+from django.views.generic import TemplateView 
 from .views import index
 
 urlpatterns = [
-    path('', index, name='index'),
     path('admin/', admin.site.urls),
     path('api/auth/', include('dj_rest_auth.urls')),
     path('api/auth/signup/', include('dj_rest_auth.registration.urls')),
     path('api/profile/', include('user_profile.urls')),
     path('api/game/', include('game.urls')),
+    re_path('(^(?!(api|admin)).*$)',index,name='index'),
 ]
 
 urlpatterns += static(settings.MEDIA_URL,
