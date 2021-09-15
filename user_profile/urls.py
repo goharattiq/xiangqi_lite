2fda83ad5b5a3e43864ad60ecf4ebdbc01d89ee8
Gohar Attiq <gohar.attiq@arbisoft.com>
2021-09-15 10:14:15 +0500

    named the urls

--- user_profile/urls.py
+++ user_profile/urls.py
@@ -2,8 +2,8 @@ from django.urls import path
 from .views import RetrieveProfile, CreateProfile, SearchUser, LeaderBoard
 
 urlpatterns = [
-    path('leaderboard/', LeaderBoard.as_view()),
-    path('search/<username>/', SearchUser.as_view()),
-    path('', CreateProfile.as_view()),
-    path('<str:pk>/', RetrieveProfile.as_view()),
+    path('leaderboard/', LeaderBoard.as_view(), name='get_leaderboard'),
+    path('search/<username>/', SearchUser.as_view(), name='search_username'),
+    path('', CreateProfile.as_view(), name='create_profile'),
+    path('<str:pk>/', RetrieveProfile.as_view(), name='retrieve_profile'),
 ]
