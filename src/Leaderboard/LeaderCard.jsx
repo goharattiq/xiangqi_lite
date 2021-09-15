758c830b706e390b780a198374a24c22350cbfbe
Gohar Attiq <gohar.attiq@arbisoft.com>
2021-09-15 12:14:56 +0500

    profile link on leaderboard added

--- src/Leaderboard/LeaderCard.jsx
+++ src/Leaderboard/LeaderCard.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 
 import PropTypes from 'prop-types';
 import { Card } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
 import './LeaderBoard.scss';
 
 const LeaderCard = ({ leader }) => {
@@ -16,7 +17,10 @@ const LeaderCard = ({ leader }) => {
     <Card className="col-lg-2 col-md-4 col-sm-3 col-5 m-3">
       <img src={leader.photo} alt={leader.user.username} className="leader-avatar m-2" />
       <Card.Body>
-        <Card.Title className="d-flex justify-content-center">{leader.user.username}</Card.Title>
+        <Link to={`/profile/${leader.user.username}`}>
+          <Card.Title className="d-flex justify-content-center">{leader.user.username}</Card.Title>
+        </Link>
+
         <Card.Text className="d-flex justify-content-center">{leader.rating}</Card.Text>
         <table className="d-flex justify-content-center">
           <tbody>
