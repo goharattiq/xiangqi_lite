142cd6d545c4b36f06843dfaae38def79ebcd41a
Gohar Attiq <gohar.attiq@arbisoft.com>
2021-09-17 17:06:53 +0500

    timer component updated

--- src/Game/Board/Timer.jsx
+++ src/Game/Board/Timer.jsx
@@ -7,11 +7,12 @@ import { socketEndGame } from '../../socketio/gameSocketio';
 import './Timer.scss';
 
 const Timer = ({
-  moveTimer, gameTimer, isPause, style, userID,
+  playerTimer, isPause, style, userID
 }) => {
+  const { move_time, game_time } = playerTimer;
   const [timer, setTimer] = useState({
-    moveInterval: moveTimer * 60,
-    gameInterval: gameTimer * 60,
+    moveInterval: move_time < game_time ?  move_time * 60 : game_time * 60,
+    gameInterval: game_time * 60,
   });
   const gameParams = useSelector(({ game }) => (game.params));
   const { moveInterval, gameInterval } = timer;
@@ -34,17 +35,21 @@ const Timer = ({
         socketEndGame(gameParams.id, {
           player_1,
           player_2,
-        }, gameParams.player_turn, 'END_TIME', gameParams.is_rated);
+        }, gameParams.player_turn, 
+        'END_TIME', 
+        gameParams.is_rated);
       }
     }
     if (isPause) {
       setTimer({
         ...timer,
-        moveInterval: gameInterval < moveTimer ? gameInterval : moveTimer * 60,
+        moveInterval: move_time < game_time ? move_time * 60: game_time * 60,
       });
       pauseTimer(timeInterval);
     }
-    return () => { pauseTimer(timeInterval); };
+    return () => { 
+      pauseTimer(timeInterval); 
+    };
   }, [moveInterval, isPause]);
 
   const timeFormatter = (string) => (new Array(2).join('0') + string).slice(-2);
@@ -64,8 +69,7 @@ const Timer = ({
 };
 
 Timer.propTypes = {
-  moveTimer: PropTypes.number.isRequired,
-  gameTimer: PropTypes.number.isRequired,
+  playerTimer: PropTypes.object.isRequired,
   isPause: PropTypes.bool.isRequired,
   style: PropTypes.object.isRequired,
   userID: PropTypes.number.isRequired,
