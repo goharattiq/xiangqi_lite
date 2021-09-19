cff73ff990f426b2f6d944eb80cef8cfd2226823
Gohar Attiq <gohar.attiq@arbisoft.com>
2021-09-19 16:12:34 +0500

    toast action updated

--- src/redux/toast/actions.js
+++ src/redux/toast/actions.js
@@ -3,14 +3,15 @@ import { v4 as uuidv4 } from 'uuid';
 import { TOAST_TIMER } from '../../utils/constants';
 import { SET_TOAST, REMOVE_TOAST } from './type';
 
-export const setToast = (msg, type, dispatch) => {
+export const setToast = (msg, type, dispatch,data) => {
+  // console.log(link)
   const id = uuidv4();
   setTimeout(() => {
     dispatch(removeToast(id));
   }, TOAST_TIMER);
   return ({
     type: SET_TOAST,
-    payload: { msg, type, id },
+    payload: { msg, type, id, data },
   });
 };
 
