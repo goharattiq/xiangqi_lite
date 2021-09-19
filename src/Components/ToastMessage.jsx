9dda835b651601a06e03d82f7ea500f7a90d8b04
Gohar Attiq <gohar.attiq@arbisoft.com>
2021-09-19 16:13:30 +0500

    toast component updated

--- src/Components/ToastMessage.jsx
+++ src/Components/ToastMessage.jsx
@@ -2,17 +2,21 @@ import React from 'react';
 
 import { Toast, ToastContainer } from 'react-bootstrap';
 import { useDispatch, useSelector } from 'react-redux';
+import { Link } from 'react-router-dom';
 
 import { removeToast } from '../redux/toast/actions';
 import './ToastMessage.scss';
 
 const ToastMessage = () => {
   const dispatch = useDispatch();
-  const toasts = useSelector(({ toast }) => (toast));
+  const { toasts,user } = useSelector(({ toast,auth }) => ({
+    toasts: toast,
+    user: auth ? auth.user : '' 
+  }));
   return (
     <ToastContainer position="top-end" className="p-3 toast-container">
       {
-        toasts.length !== 0 ? toasts.map(({ msg, type, id }) => (
+        toasts.length !== 0 ? toasts.map(({ msg, type, id, data }) => (
           <Toast
             animation
             onClose={() => dispatch(removeToast(id))}
@@ -20,7 +24,23 @@ const ToastMessage = () => {
             key={id}
           >
             <Toast.Header />
-            <Toast.Body>{msg}</Toast.Body>
+            <Toast.Body>
+              {
+                data && data.creator !== user.username ?  (
+                  <>
+                    <p>{`${data.creator} created the game.`}</p>
+                    {
+                      user.username === data.invitee ? <p>{`You are challenged in this game.`}</p> : ''
+                    }
+                    <Link to={`game/${data.gameID}`}>
+                      {
+                        user.username === data.invitee ? 'join' : 'view'
+                      }
+                    </Link>
+                  </>
+                ) : msg
+              }
+            </Toast.Body>
           </Toast>
         )) : ''
       }
