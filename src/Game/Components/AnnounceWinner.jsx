c8711e87282bc7f399f9d9e1474d4ea80a685661
Gohar Attiq <gohar.attiq@arbisoft.com>
2021-09-21 21:45:58 +0500

    announce winner componet updated

--- src/Game/Components/AnnounceWinner.jsx
+++ src/Game/Components/AnnounceWinner.jsx
@@ -5,24 +5,28 @@ import { Button } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 import './AnnounceWinner.scss';
 
-const AnnounceWinner = ({ player, username }) => (
-  <div className="position-absolute w-100 h-100 overlay-div">
-    <div className="position-absolute bg-white pt-4 winner">
-      <p className="text-center">
-        {`${player.profile.user.username === username ? 'You' : player.profile.user.username} Won`}
-      </p>
-      <Link to="/lobby">
-        <Button className="position-relative m-4 form-button">
-          Back To Lobby
-        </Button>
-      </Link>
-    </div>
-  </div>
+const AnnounceWinner = ({ player, username, announce }) => (
+  announce
+    ? (
+      <div className="position-absolute w-100 h-100 overlay-div">
+        <div className="position-absolute bg-white pt-4 winner">
+          <p className="text-center">
+            {`${player.profile.user.username === username ? 'You' : player.profile.user.username} Won`}
+          </p>
+          <Link to="/lobby">
+            <Button className="position-relative m-4 form-button">
+              Back To Lobby
+            </Button>
+          </Link>
+        </div>
+      </div>
+    ) : ''
 );
 
 AnnounceWinner.propTypes = {
   player: PropTypes.object.isRequired,
   username: PropTypes.string.isRequired,
+  announce: PropTypes.bool.isRequired,
 };
 
 export default AnnounceWinner;
